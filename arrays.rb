6aa941ddbf18877d256ce3fbf89f4a5b3023f1ee
James White <whitejaad@gmail.com>
2022-07-10 12:02:24 -0700

    Arrays,save2

--- arrays.rb
+++ arrays.rb
@@ -1,3 +1,10 @@
+friend1 = "Allan"
+friend2 = "Joel"
+friend3 = "Wes"
+friend4 = "Shane"
+friend3 = "Jon"
+
+
 friends = "Allan", "Joel", "Wes", "Shane", "Jon"
 
 length_of_friendship = 13.5,14.2,13.1,38.2,36.4
@@ -5,3 +12,5 @@ length_of_friendship = 13.5,14.2,13.1,38.2,36.4
 Kids = Jon3,Joel1,Allan4,Wes1,Jon3
 
 code_name = Brothers_by_choice
+
+puts code_name
