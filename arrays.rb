fa4e7bcde3dfde252017c31fc22eef9c7753817e
James White <whitejaad@gmail.com>
2022-07-10 19:03:36 -0700

    Arrays puts update

--- arrays.rb
+++ arrays.rb
@@ -11,6 +11,6 @@ length_of_friendship = 13.5,14.2,13.1,38.2,36.4
 
 Kids = Jon3,Joel1,Allan4,Wes1,Shane2
 
-code_name = Brothers_by_choice
+code_name = "Brothers_by_choice"
 
-puts code_name
+puts code_name = "Brothers by choice"
